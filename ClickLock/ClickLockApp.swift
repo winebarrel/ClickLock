63946c1ca51f114592834718ce4a5af7f2b35d69
winebarrel <sugawara@winebarrel.jp>
2024-03-09 14:11:09 +0900

    first commit

---
+++ ClickLock/ClickLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickLockApp.swift
+//  ClickLock
+//
+//  Created by 菅原元気 on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct ClickLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClickLock
+//
+//  Created by 菅原元気 on 2024/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
