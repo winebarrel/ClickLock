ae6c0226eff5958986926d4eb88a5a97b38aead8
winebarrel <sugawara@winebarrel.jp>
2024-03-09 14:35:13 +0900

    Remove header comment

--- ClickLock/ClickLockApp.swift
+++ ClickLock/ClickLockApp.swift
@@ -1,10 +1,3 @@
-//
-//  ClickLockApp.swift
-//  ClickLock
-//
-//  Created by 菅原元気 on 2024/03/09.
-//
-
 import SwiftUI
 
 @main
--- ClickLock/ContentView.swift
+++ ClickLock/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  ClickLock
-//
-//  Created by 菅原元気 on 2024/03/09.
-//
-
 import SwiftUI
 
 struct ContentView: View {
